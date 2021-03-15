f4c9beab6d295d83aa65779d60b75cac959f0b62
Shamsiddin <47350966+Shamsiddin94@users.noreply.github.com>
2021-03-15 15:34:59 +0500

    ready

--- src/main/java/exam/demo/entity/enums/FileType.java
+++ src/main/java/exam/demo/entity/enums/FileType.java
@@ -3,11 +3,9 @@ package exam.demo.entity.enums;
 public enum FileType {
     DOCUMENT,
     PICTURE,
-<<<<<<< Updated upstream
     SENDDOC,
-    SENDPIC
-=======
+    SENDPIC,
     SEND_DOCUMENT,
     SEND_PICTURE
->>>>>>> Stashed changes
+
 }

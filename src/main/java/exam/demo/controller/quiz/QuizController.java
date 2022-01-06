1285629896c1bcbc8ddca109a704053d49d35af2
Shamsiddin <47350966+Shamsiddin94@users.noreply.github.com>
2022-01-06 10:23:57 +0500

    config ignore constant and properties

---
+++ src/main/java/exam/demo/controller/quiz/QuizController.java
@@ -0,0 +1,15 @@
+package exam.demo.controller.quiz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/quiz")
+public class QuizController {
+
+
+
+
+
+
+}
---
+++ src/main/java/exam/demo/service/quizService/QuizService.java
@@ -0,0 +1,9 @@
+package exam.demo.service.quizService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+
+
+}
